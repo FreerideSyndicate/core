4cbab8854017b6e7cfc8ea5869ad9d6335179582
GregPrice24 <Gprice2432@gmail.com>
2017-01-06 13:58:34 -0600

    start restructure of mongo interface/repository

--- src/main/java/freeride/co/services/MongoService.java
+++ src/main/java/freeride/co/services/MongoService.java
@@ -6,16 +6,8 @@ package freeride.co.services;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-interface Mongo<T> {
-    default boolean save(T document) {return false;};
-    default boolean create(T document) {return false;};
-    default boolean findOneAndUpdate(T query, ArrayList<T> fields) {return false;};
-    default T find(T query) {return null;};
-    default boolean findOneAndRemove(T query) {return false;};
-}
+import org.springframework.stereotype.Service;
 
 @Service
 public class MongoService {
