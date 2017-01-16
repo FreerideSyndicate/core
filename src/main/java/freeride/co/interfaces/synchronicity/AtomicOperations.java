93e7cd45332b2bfdda3378ab40075d772c09663f
GregPrice24 <Gprice2432@gmail.com>
2017-01-16 14:02:48 -0600

    begin stubbing synchronized interfaces

--- src/main/java/freeride/co/interfaces/synchronicity/AtomicOperations.java
+++ src/main/java/freeride/co/interfaces/synchronicity/AtomicOperations.java
@@ -1,7 +1,20 @@
 package freeride.co.interfaces.synchronicity;
 
+
+import org.springframework.util.concurrent.FailureCallback;
+import org.springframework.util.concurrent.SuccessCallback;
+
+import java.util.concurrent.Future;
+import java.util.function.Function;
+
 /**
  * Created by Greg on 1/15/17.
  */
+
 public interface AtomicOperations<T> {
+
+    default void schedule(SuccessCallback<T> task, FailureCallback completion) {};
+
+    default Future<T> generateExecutable(Function<T, Throwable> executable) {return null;};
+
 }
--- src/main/java/freeride/co/interfaces/synchronicity/ParallelOperations.java
+++ src/main/java/freeride/co/interfaces/synchronicity/ParallelOperations.java
@@ -4,7 +4,6 @@ package freeride.co.interfaces.synchronicity;
  * Created by Greg on 1/15/17.
  */
 
-
 public interface ParallelOperations<T extends AtomicOperations<? super T>> {
 
 }
